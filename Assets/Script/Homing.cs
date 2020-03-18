8386a35f66a66a49be4e5a7b7348a22724b3373b
山下 翔太郎 <nkc.syotaro.yamashita@gmail.com>
2020-03-19 00:19:30 +0900

    ミサイルとりあえず完成（ホーミング）

--- Assets/Script/Homing.cs
+++ Assets/Script/Homing.cs
@@ -6,13 +6,16 @@ using UnityEngine;
 public class Homing : MonoBehaviour
 {
     private GameObject player, enemy;
-    private float time;
-    private Vector3 velocity, position;
     private Transform target;
-    private float period = 2f;
-    Rigidbody rigid;
-    public Vector3 acceleration;
-
+    private int routeChoice;    //プレイヤーを狙うミサイルか否か
+    private int destroyTime = 5;    //消滅時間
+    private float destroyTimer; //消滅時間を測る
+    private float bulletPeriod = 3f;  //弾着
+    private float keepWatching = 10f;  //相手を向かなくなる距離
+    private Vector3 velocity;   //速度
+    private Vector3 acceleration;   //加速度
+    private Vector3 missileDistance;   //相手とミサイルの距離
+    private Rigidbody rigid;
 
     // Start is called before the first frame update
     void Start()
@@ -20,23 +23,43 @@ public class Homing : MonoBehaviour
         rigid = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
         enemy = GameObject.Find("Enemy");
-        velocity = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0);
+        routeChoice = Random.Range(0, 2);    //ターゲットを狙うか否か
+        //velocity = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0); //初速度
+        //Debug.Log(velocity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
-        //position = enemy.transform.position;
+
+        destroyTimer += Time.deltaTime;
         target = enemy.transform;
 
         acceleration = Vector3.zero;
 
-        //ターゲットと自分自身の差
-        var diff = target.position - transform.position;
+        if (routeChoice == 0)   //0 :狙う , 1 :狙わない
+        {
+            missileDistance = target.position - transform.position; //間の距離を代入
+
+            if (missileDistance.z >= keepWatching)  //ターゲットを見続ける
+            {
+                var look = Quaternion.LookRotation(missileDistance);
+                transform.localRotation = look;
+            }
+            else
+            {
+                rigid.constraints = RigidbodyConstraints.FreezeRotation;    //角度を固定
+            }
+
+        }
+        else
+        {
+            missileDistance = target.position - player.transform.position;  //間の距離を代入
+            rigid.constraints = RigidbodyConstraints.FreezeRotation;    //角度を代入
+        }
 
         //加速度
-        acceleration += (diff - velocity * period) * 2f / (period * period);
+        acceleration += (missileDistance - velocity * bulletPeriod) * 2f / (bulletPeriod * bulletPeriod);
 
         //加速度が一定以上だと追尾を弱くする
         if (acceleration.magnitude > 100f)
@@ -45,12 +68,12 @@ public class Homing : MonoBehaviour
         }
 
         // 着弾時間を徐々に減らしていく
-        period -= Time.deltaTime;
+        bulletPeriod -= Time.deltaTime;
 
         // 速度の計算
         velocity += acceleration * Time.deltaTime;
 
-        if (time >= 5)
+        if (destroyTimer >= destroyTime)    //消滅
             Destroy(gameObject);
 
         //Debug.Log(rigid);
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,10 +6,11 @@ public class Player : MonoBehaviour
 {
     public GameObject bullet;   //弾
     private Vector3 playerPosition;
+    private const int BLAZEFIRE= 5;  //一度に発射する数
+    private Vector3[] missileAppear = new Vector3[BLAZEFIRE];   //出現位置格納
     private float timer, flagTimer; //次弾までのクールタイム , 初弾クールタイム
     private float span = 1.5f;  //連射スパン
     private bool bulletFlag;    //初弾フラグ
-    private int blazeFire = 5;  //一度に発射する数
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +21,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        for (int i = 0; i < BLAZEFIRE; i++) //ランダムな出現位置を格納
+        {
+            missileAppear[i] = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0);
+        }
+
         timer += Time.deltaTime;
         if (Input.GetKey(KeyCode.Z))
         {
@@ -28,15 +34,16 @@ public class Player : MonoBehaviour
             if(timer >= span || bulletFlag == false)
             {
                 bulletFlag = true;
-                for (int i = 0; i < blazeFire; i++)
+                for (int i = 0; i < BLAZEFIRE; i++) //決めた数ミサイル出す
                 {
-                    Instantiate(bullet, playerPosition, Quaternion.identity);
+                    Instantiate(bullet, missileAppear[i], Quaternion.identity);
+                    //Instantiate(bullet, playerPosition, Quaternion.identity);
                 }
                 flagTimer = 0f;
                 timer = 0f;
             }
         }
-        else if (bulletFlag == true)
+        else if (bulletFlag == true)    //初弾処理
         {
             flagTimer += Time.deltaTime;
             if (flagTimer >= span)
@@ -44,7 +51,7 @@ public class Player : MonoBehaviour
                 bulletFlag = false;
             }
         }
-        else
+        else    //タイマーリセット
         {
             timer = 0;
         }
